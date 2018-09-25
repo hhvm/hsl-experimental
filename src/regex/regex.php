ea23715921f55f84da9ddb03a6e13067b8df821c
Kunal Mehta <kunalm@fb.com>
2018-09-25 13:33:40 -0700

    Split Regex implementation into separate files
    Summary: This moves `Regex\match_base` to `_Private\regex_match`, and moves the Exception class into its own file.
    
    Reviewed By: fredemmott
    
    Differential Revision: D10033028
    
    fbshipit-source-id: 6701c421caaaf1c68b21b88eaf5b8875a8500d30

--- src/regex/regex.php
+++ src/regex/regex.php
@@ -10,69 +10,7 @@
 
 namespace HH\Lib\Regex;
 
-use namespace HH\Lib\Str;
-
-final class Exception extends \Exception {
-  public function __construct<T>(Pattern<T> $pattern): void {
-    static $errors = dict[
-      \PREG_INTERNAL_ERROR => 'Internal error',
-      \PREG_BACKTRACK_LIMIT_ERROR => 'Backtrack limit error',
-      \PREG_RECURSION_LIMIT_ERROR => 'Recursion limit error',
-      \PREG_BAD_UTF8_ERROR => 'Bad UTF8 error',
-      \PREG_BAD_UTF8_OFFSET_ERROR => 'Bad UTF8 offset error',
-    ];
-    parent::__construct(
-      Str\format(
-        "%s: %s",
-        idx($errors, \preg_last_error(), 'Invalid pattern'),
-        /* HH_FIXME[4110] Until we have a to_string() function */
-        $pattern,
-      ),
-    );
-  }
-}
-
-// TODO(T19708752): Tag appropriate functions as reactive once we've gotten rid of preg_match
-
-/**
- * Temporary stand-in for native match function to be implemented in T30991246.
- * Returns the first match found in `$haystack` given the regex pattern `$pattern`
- * and an optional offset at which to start the search.
- *
- * Throws Invariant[Violation]Exception if `$offset` is not within plus/minus the length of `$haystack`
- * Returns null, or a tuple of
- * first,
- *   a Match containing
- *     - the entire matching string, at key 0,
- *     - the results of unnamed capture groups, at integer keys corresponding to
- *         the groups' occurrence within the pattern, and
- *     - the results of named capture groups, at string keys matching their respective names,
- * and second,
- *   the integer offset at which this first match occurs in the haystack string.
- */
-function match_base<T as Match>(
-  string $haystack,
-  Pattern<T> $pattern,
-  int $offset = 0,
-): ?(T, int) {
-  $offset = \HH\Lib\_Private\validate_offset($offset, Str\length($haystack));
-  $match = darray[];
-  $status =
-    @\preg_match($pattern, $haystack, &$match, \PREG_OFFSET_CAPTURE, $offset);
-  if ($status === 1) {
-    $match_out = darray[];
-    foreach ($match as $key => $value) {
-      $match_out[$key] = $value[0];
-    }
-    $offset_out = $match[0][1];
-    /* HH_FIXME[4110] Native function won't have this problem */
-    return tuple($match_out, $offset_out);
-  } else if ($status === 0) {
-    return null;
-  } else {
-    throw new Exception($pattern);
-  }
-}
+use namespace HH\Lib\{_Private, Str};
 
 /**
  * Returns the first match found in `$haystack` given the regex pattern `$pattern`
@@ -90,7 +28,7 @@ function match<T as Match>(
   Pattern<T> $pattern,
   int $offset = 0,
 ): ?T {
-  return match_base($haystack, $pattern, $offset)[0] ?? null;
+  return _Private\regex_match($haystack, $pattern, $offset)[0] ?? null;
 }
 
 /**
@@ -105,7 +43,7 @@ function match_all<T as Match>(
   int $offset = 0,
 ): \Generator<int, T, void> {
   $haystack_length = Str\length($haystack);
-  while ($match = match_base($haystack, $pattern, $offset)) {
+  while ($match = _Private\regex_match($haystack, $pattern, $offset)) {
     $captures = $match[0];
     yield $captures;
     $match_begin = $match[1];
@@ -134,7 +72,7 @@ function matches<T as Match>(
   Pattern<T> $pattern,
   int $offset = 0,
 ): bool {
-  return match_base($haystack, $pattern, $offset) !== null;
+  return _Private\regex_match($haystack, $pattern, $offset) !== null;
 }
 
 /**
@@ -154,8 +92,8 @@ function replace<T as Match>(
   // function other than preg_match. It needs to call into preg_replace
   // to be able to handle backreferencing in the `$replacement` string.
   // preg_replace does not support offsets, so we handle them ourselves,
-  // consistently with match_base.
-  $offset = \HH\Lib\_Private\validate_offset($offset, Str\length($haystack));
+  // consistently with _Private\regex_match.
+  $offset = _Private\validate_offset($offset, Str\length($haystack));
   $haystack1 = Str\slice($haystack, 0, $offset);
   $haystack2 = Str\slice($haystack, $offset);
 
@@ -182,7 +120,7 @@ function replace_with<T as Match>(
   $haystack_length = Str\length($haystack);
   $result = Str\slice($haystack, 0, 0);
   $match_end = 0;
-  while ($match = match_base($haystack, $pattern, $offset)) {
+  while ($match = _Private\regex_match($haystack, $pattern, $offset)) {
     $captures = $match[0];
     $match_begin = $match[1];
     // Copy anything between the previous match and this one
@@ -235,7 +173,7 @@ function split<T as Match>(
   $offset = 0;
   $match_end = 0;
   $count = 1;
-  $match = match_base($haystack, $delimiter, $offset);
+  $match = _Private\regex_match($haystack, $delimiter, $offset);
   while ($match && $count < $limit) {
     $captures = $match[0];
     $match_begin = $match[1];
@@ -257,7 +195,7 @@ function split<T as Match>(
       $offset = $match_end;
     }
     $count++;
-    $match = match_base($haystack, $delimiter, $offset);
+    $match = _Private\regex_match($haystack, $delimiter, $offset);
   }
   $result[] = Str\slice($haystack, $match_end);
   return $result;
