31c42e210b09b920c0db8a82748da27d6b69abc3
Fred Emmott <fe@fb.com>
2018-12-03 13:45:17 -0800

    Remove stdout/stderr/stdin, add {request,server}{Output,Input,Error} (#27)
    Summary:
    fixes #26
    
    STDIN, STDOUT, and STDERR are undefined except in CLI mode. The php://std* handles are always *server* file handles. Expose php://input and php://output as well
    
    Pull Request resolved: https://github.com/hhvm/hsl-experimental/pull/27
    
    Reviewed By: alexeyt
    
    Differential Revision: D13304206
    
    Pulled By: alexeyt
    
    fbshipit-source-id: 94b8cfb303e8a70c72dbb015a938b6c2f4dfdd3f

--- src/io/stdio.php
+++ src/io/stdio.php
@@ -12,14 +12,60 @@ namespace HH\Lib\Experimental\IO;
 
 use namespace HH\Lib\_Private;
 
-function stdout(): WriteHandle {
-  return _Private\StdioHandle::stdout();
+/** Return STDOUT for the server process.
+ *
+ * This is usually not the same thing as request output.
+ *
+ * @see request_output
+ */
+function server_output(): WriteHandle {
+  return _Private\StdioHandle::serverOutput();
 }
 
-function stderr(): WriteHandle {
-  return _Private\StdioHandle::stderr();
+/** Return STDERR for the server process.
+ *
+ * @see request_error
+ */
+function server_error(): WriteHandle {
+  return _Private\StdioHandle::serverError();
 }
 
-function stdin(): ReadHandle {
-  return _Private\StdioHandle::stdin();
+/** Return STDIN for the server process.
+ *
+ * @see request_input
+ */
+function server_input(): ReadHandle {
+  return _Private\StdioHandle::serverInput();
+}
+
+/** Return the output handle for the current request.
+ *
+ * This should generally be used for sending data to clients. In CLI mode, this
+ * is usually the process STDOUT.
+ *
+ * @see requestOutput
+ */
+function request_output(): WriteHandle {
+  return _Private\StdioHandle::requestOutput();
+}
+
+/** Return the error output handle for the current request.
+ *
+ * This is usually only available for CLI scripts; it will throw an
+ * `UnsupportedHandleException` in most other contexts, including HTTP
+ * requests.
+ *
+ * In CLI mode, this is usually the process STDERR.
+ */
+function request_error(): WriteHandle {
+  return _Private\StdioHandle::requestError();
+}
+
+/** Return the input handle for the current request.
+ *
+ * In CLI mode, this is likely STDIN; for HTTP requests, it may contain the
+ * POST data, if any.
+ */
+function request_input(): ReadHandle {
+  return _Private\StdioHandle::requestInput();
 }
