3910231076f98a1811dd142d02593dd295e82575
Saif Eddin G <29315886+azjezz@users.noreply.github.com>
2019-03-04 21:58:09 +0100

    Update FileWriteMode.php

--- src/filesystem/FileWriteMode.php
+++ src/filesystem/FileWriteMode.php
@@ -18,26 +18,26 @@ enum FileWriteMode: string {
    * If the file exits, it is not truncated (as with `TRUNCATE`), and the call
    * suceeds (unlike `EXCLUSIVE_CREATE`).
    */
-  OPEN_OR_CREATE = 'c';
+  OPEN_OR_CREATE = 'cb';
 
   /**
    * Open for writing only; place the file pointer at the beginning of the
    * file and truncate the file to zero length. If the file does not exist,
    * attempt to create it.
    */
-  TRUNCATE = 'w';
+  TRUNCATE = 'wb';
 
   /**
    * Open for writing only; place the file pointer at the end of the file. If
    * the file does not exist, attempt to create it. In this mode, seeking has
    * no effect, writes are always appended.
    */
-  APPEND = 'a';
+  APPEND = 'ab';
 
   /**
    * Create and open for writing only; place the file pointer at the beginning
    * of the file. If the file already exists, the filesystem call will throw an
    * exception. If the file does not exist, attempt to create it.
    */
-  MUST_CREATE = 'x';
+  MUST_CREATE = 'xb';
 }
