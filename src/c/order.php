dc4adf9b27b390ed91afccd4760b0f5c5170ea80
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2019-11-01 17:07:55 +0100

    Add is_sorted_by and correct the doc of is_sorted

--- src/c/order.php
+++ src/c/order.php
@@ -19,11 +19,14 @@ namespace HH\Lib\C;
  * This will sort numbers by value, strings by alphabetical order
  * and DateTime/DateTimeImmutable by their unixtime.
  *
+ * To check the order of other types or mixtures of the
+ * aforementioned types, see C\is_sorted_by.
+ *
  * If the comparison operator `<=>` is not useful on Tv
  * and no $comparator is provided, the result of is_sorted
  * will not be useful.
  *
- * Time complexity: O((n log n) * c), where c is the complexity of the
+ * Time complexity: O((n * c), where c is the complexity of the
  * comparator function (which is O(1) if not provided explicitly)
  * Space complexity: O(n)
  */
@@ -43,6 +46,53 @@ function is_sorted<Tv>(
 
   $previous = firstx($vec);
   foreach ($vec as $next) {
+    if ($comparator($next, $previous) < 0) {
+      return false;
+    }
+    $previous = $next;
+  }
+
+  return true;
+}
+
+/**
+ * Returns true if the given Traversable<Tv> would be sorted in ascending order
+ * after having been `Vec\map`ed with $scalar_func sorted in ascending order.
+ * If two neighbouring elements compare equal, this will be considered sorted.
+ *
+ * If no $comparator is provided, the `<=>` operator will be used. 
+ * This will sort numbers by value, strings by alphabetical order
+ * and DateTime/DateTimeImmutable by their unixtime.
+ *
+ * To check the order without a mapping function,
+ * see `C\is_sorted`.
+ *
+ * If the comparison operator `<=>` is not useful on Ts
+ * and no $comparator is provided, the result of is_sorted_by
+ * will not be useful.
+ *
+ * Time complexity: O((n * c), where c is the complexity of the
+ * comparator function (which is O(1) if not provided explicitly)
+ * Space complexity: O(n)
+ */
+<<__Rx, __AtMostRxAsArgs, __ProvenanceSkipFrame>>
+function is_sorted_by<Tv, Ts>(
+  <<__MaybeMutable, __OnlyRxIfImpl(\HH\Rx\Traversable::class)>>
+  Traversable<Tv> $traversable,
+  <<__AtMostRxAsFunc>> (function(Tv): Ts) $scalar_func,
+  <<__AtMostRxAsFunc>> ?(function(Ts, Ts): int) $comparator = null,
+): bool {
+  $vec = vec($traversable);
+  if (is_empty($vec)) {
+    return true;
+  }
+
+  $comparator ??= ($a, $b) ==>
+    /*HH_IGNORE_ERROR[4240] Comparison may not be useful on Ts*/$a <=> $b;
+
+  $previous = $scalar_func(firstx($vec));
+  foreach ($vec as $next) {
+    $next = $scalar_func($next);
     if ($comparator($next, $previous) < 0) {
       return false;
     }
