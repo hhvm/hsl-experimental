951031d0540104dd2fee2af3b3894a32d5b4632c
Fred Emmott <fe@fb.com>
2020-07-04 08:47:21 -0700

    Add OS\mkdtemp
    I don't usually add tests for `OS\` given they're almost entirely tested
    by the HHVM built-in tests, and by `File\`, `TCP\`, `UDP\` etc; I'm
    adding one here given that it's not yet clear what the higher-level API
    (if any) will be, so there isn't any coverage there.

---
+++ tests/os/MkdtempTest.php
@@ -0,0 +1,47 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the MIT license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
+ */
+
+use namespace HH\Lib\{IO, OS, Str};
+
+use function Facebook\FBExpect\expect; // @oss-enable
+use type Facebook\HackTest\HackTest; // @oss-enable
+// @oss-disable: use type HackTest;
+
+// @oss-disable: <<Oncalls('hack')>>
+final class MkdtempTest extends HackTest {
+  public function testBasicUsage(): void {
+    $pattern = Str\strip_suffix(sys_get_temp_dir(), '/').'/hsl-test-XXXXXX';
+    $tempdir = OS\mkdtemp($pattern);
+    expect($tempdir)->toNotEqual(
+      $pattern,
+      'expected literal `X` to be replaced',
+    );
+    $prefix = Str\strip_suffix($pattern, 'XXXXXX');
+    expect(Str\starts_with($tempdir, $prefix))->toBeTrue(
+      'tempdir and pattern do not share a prefix',
+    );
+    expect(is_dir($tempdir))->toBeTrue();
+    expect((stat($tempdir)['mode']) & 0777)->toEqual(0700);
+    rmdir($tempdir);
+  }
+
+  public function testTooFewPlaceholders(): void {
+    $pattern = Str\strip_suffix(sys_get_temp_dir(), '/').'/hsl-test-XXX';
+    $ex = expect(() ==> OS\mkdtemp($pattern))->toThrow(
+      OS\ErrnoException::class,
+    );
+    expect($ex->getErrno())->toEqual(OS\Errno::EINVAL);
+  }
+
+  public function testNoParentDirectory(): void {
+    $pattern = '/idonotexist/foo.XXXXXX';
+    expect(() ==> OS\mkdtemp($pattern))->toThrow(OS\NotFoundException::class);
+  }
+}
